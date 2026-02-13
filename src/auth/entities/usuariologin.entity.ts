f032afce57f9daa61893881abe5039c0fbc23ead
Giovanna Roberta <giiroberta18@gmail.com>
2026-02-13 18:20:53 -0300

    Deploy

--- src/auth/entities/usuariologin.entity.ts
+++ src/auth/entities/usuariologin.entity.ts
@@ -2,8 +2,8 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class UsuarioLogin {
   @ApiProperty()
-  public usuario: string;
+  public usuario!: string;
 
   @ApiProperty()
-  public senha: string;
+  public senha!: string;
 }
